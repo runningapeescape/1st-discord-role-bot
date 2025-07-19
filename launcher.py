9c20ac935f4b4ef43c1285d10d207c2f07735c41
runningapeescape <comon.spool689@slmails.com>
2025-07-19 06:03:48 +0300

    launcher.py

--- launcher.py
+++ launcher.py
@@ -1,7 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 A boot-script to start the bot without installing as package
 """
+import sys
+import os
+
+# Ensure 'src' is in the import path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
+from bot import start_bot
+
 if __name__ == '__main__':
-    from src.bot import start_bot
     start_bot()
